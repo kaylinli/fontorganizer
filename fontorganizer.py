2312d8541d636d30acb53ac35ab818a969401de8
kaylinli <53351309+kaylinli@users.noreply.github.com>
2020-10-02 23:01:33 -0700

    Implementation of pages

--- fontorganizer.py
+++ fontorganizer.py
@@ -6,16 +6,49 @@ list = []
 for file in os.listdir(r'C:\Windows\Fonts'):
     if file.endswith(".ttf"):
         list.append(file)
+    if file.endswith(".otf"):
+        list.append(file)
 
 print(list)
 
-def redrawAll(app, canvas):
-    count = 0
-    for file in list:
+def appStarted(app):
+    app.entryHeight = 20
+    app.fontEntries = app.width//20 - 2# number of entries on a page
+    app.pageNum = 0
+    app.forwardButtonX = app.width/2 + 20
+    app.forwardButtonY = app.height - 10
+    app.backButtonX = app.width/2 - 20
+    app.backButtonY = app.height - 10
+
+def mousePressed(app, event):
+    if app.forwardButtonX-20 < event.x < app.forwardButtonX+20:
+        app.pageNum += 1
+    if app.backButtonX-20 < event.x < app.backButtonX+20:
+        if app.pageNum != 0:
+            app.pageNum -= 1
+
+def pageSetup(app, canvas):
+    count = app.entryHeight
+    firstEntry = app.pageNum * app.fontEntries
+    for file in list[firstEntry:(firstEntry+app.fontEntries)]:
         try:
-            canvas.create_text(app.width/2, count, text=f'{file}')
-        except:
-            print(f'{file}')
-        count +=20
+            fontType = str(file)
+            fontType = fontType[:(len(fontType)-4)]
+            print(fontType)
+            fontType.replace(" ", "_")
+            print(fontType)
+            canvas.create_text(app.width/2, count, text=f'{file}', font=fontType)
+        except Exception as e:
+            print(e)
+            # print(f'{fontType}')
+        count += app.entryHeight
+    
+def redrawAll(app, canvas):
+    pageSetup(app, canvas)
+    canvas.create_rectangle(app.forwardButtonX-10, app.forwardButtonY-10, app.forwardButtonX+10,app.forwardButtonY+10)
+    canvas.create_rectangle(app.backButtonX-10, app.backButtonY-10, app.backButtonX+10,app.backButtonY+10)
+    canvas.create_text(app.forwardButtonX, app.forwardButtonY, text='>')
+    canvas.create_text(app.backButtonX, app.backButtonY, text='<')
+    
 
 runApp(width = 500, height = 500)
