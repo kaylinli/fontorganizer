a8dadd453192d2021bd730ddcfa889cabedc3448
kaylinli <53351309+kaylinli@users.noreply.github.com>
2020-12-03 16:44:31 -0800

    Preliminary font explorer/ font board

--- fontexplorer.py
+++ fontexplorer.py
@@ -3,6 +3,8 @@ from tkinter import font
 import tkinter as tk
 from cmu_112_graphics import *
 
+import random
+
 # import other files
 import utility as util
 
@@ -10,7 +12,11 @@ import utility as util
 Model
 '''
 
-def initFEvars(self, canvas):
+def initFEvars(self):
+    self.widgets = [] # list of widgets
+    self.selectedWidget = 0
+    self.widgetText = []
+    self.widgetText +=["hello"]
     pass
 
 '''
@@ -23,11 +29,20 @@ def mousePressed(self, event):
 View
 '''
 
-def createHeader(self, canvas):
-    canvas.create_text(self.marginLeft, 30, text="font tagger", anchor="w", font=("Red Hat Display Medium", 24))
+def drawWidgets(self, canvas):
+    width = random.randrange(100,200)
+    height = random.randrange(100,200)
+    drawTextWidget(self, canvas, 0, self.marginLeft, 80, self.marginLeft + width, 80 + height)
+
+def drawTextWidget(self, canvas, widgetNum, x0, y0, x1, y1):
+    canvas.create_rectangle(x0, y0, x1, y1)
+    canvas.create_text((x0+x1)/2, (y0+y1)/2, text=self.widgetText[widgetNum])
     
-    drawTagInputBox(self, canvas)
+
+def createHeader(self, canvas):
+    canvas.create_text(self.marginLeft, 30, text="font explorer", anchor="w", font=("Red Hat Display Medium", 24))
 
 
 def drawFontExplorerUI(self, canvas):
-    pass
+    createHeader(self, canvas)
+    drawWidgets(self, canvas)
